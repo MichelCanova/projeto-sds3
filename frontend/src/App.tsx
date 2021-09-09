ba5659c7e0bafb6c21e1e368377291e2eb603dae
Michel Canova <90348376+MichelCanova@users.noreply.github.com>
2021-09-09 16:03:12 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import Navbar from "componets/Navbar";
+import Footer from "componets/Navbar/Footer";
+import DataTable from "componets/DataTable";
 function App() {
   return (
-    <div>
-     <h1 className="text-primary">Ola mundo!!!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Ola mundo!!!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
